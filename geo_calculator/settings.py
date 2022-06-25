3cb0c114bfe26b204546918971033e5eabd27bb8
JonasFiechter <jonasrfiechter@gmail.com>
2022-06-25 20:34:27 -0300

    .

--- geo_calculator/settings.py
+++ geo_calculator/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'geo_calculator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'geo_calculator/templates/partials'],
+        'DIRS': [BASE_DIR / 'geo_calculator/templates/',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'templates/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
